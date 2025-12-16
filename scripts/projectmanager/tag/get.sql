9497b0c6c7ebfcb1e059ce495d972857d12b15fe
Charlie Bergdall <charliusb@gmail.com>
2025-12-16 11:13:17 -0600

    attempt 2

--- scripts/projectmanager/tag/get.sql
+++ scripts/projectmanager/tag/get.sql
@@ -3,10 +3,10 @@ SELECT T.TagID,
     T.Implements::jsonb,
     T.IsPublic,
     C.Name AS OwnerName,
-    C.ContributorID AS OwnerID,
+    T.Owner,
     COUNT(DISTINCT PT.ProjectTagID) AS NumImplementations
 FROM Tags T
 LEFT JOIN Contributors C ON C.ContributorID = T.Owner
 LEFT JOIN ProjectTag PT ON PT.TagID = T.TagID
 WHERE T.TagID = %(TagID)s
-GROUP BY T.TagID, T.Title, T.Implements::jsonb, T.IsPublic, OwnerName, OwnerID;
+GROUP BY T.TagID, T.Title, T.Implements::jsonb, T.IsPublic, OwnerName, T.Owner;
